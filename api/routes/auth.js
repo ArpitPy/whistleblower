f533bdb79bf65cb409795d67de60670c74d3ce8d
Arpit Pandey <71400381+ArpitPy@users.noreply.github.com>
2022-10-02 22:01:45 +0530

    fixed login.jsx

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -28,11 +28,9 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).json("user not found");
-
+    if(!user) return res.status(404).json("user not found");
     const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("wrong password")
-
+    if(!validPassword) return res.status(400).json("wrong password")
     res.status(200).json(user)
   } catch (err) {
     res.status(500).json(err)
